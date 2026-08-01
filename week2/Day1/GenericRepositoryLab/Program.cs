e5066234c5db5c8f2ac498fa2fa4695a891da622
aboodatatra <abdatatri@gmail.com>
2026-08-01 19:50:11 +0300

    Week 2 - Day 1: Generic Repository Lab

---
+++ week2/Day1/GenericRepositoryLab/Program.cs
@@ -0,0 +1,38 @@
+﻿using GenericRepositoryLab.Models;
+using GenericRepositoryLab.Repositories;
+
+var studentRepository = new Repository<Student>();
+
+studentRepository.Add(new Student { Id = 1, Name = "Layth" });
+studentRepository.Add(new Student { Id = 2, Name = "Ahmad" });
+
+Console.WriteLine("Students:");
+var students = studentRepository.GetAll();
+
+foreach (var student in studentRepository.GetAll())
+{
+    Console.WriteLine($"{student.Id} - {student.Name}");
+}
+
+Console.WriteLine();
+
+var productRepository = new Repository<Product>();
+
+productRepository.Add(new Product { Id = 1, Name = "Laptop", Price = 3500 });
+productRepository.Add(new Product { Id = 2, Name = "Mouse", Price = 80 });
+
+Console.WriteLine("Products:");
+
+foreach (var product in productRepository.GetAll())
+{
+    Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+}
+
+Console.WriteLine();
+
+var foundStudent = studentRepository.Find(s => s.Id == 2);
+
+if (foundStudent != null)
+{
+    Console.WriteLine($"Found Student: {foundStudent.Name}");
+}
